31e0106c50e7a17d31e8c4f9c63566aba2a887f0
Jason Bouffard <jasonbouffard@gmail.com>
2013-06-16 19:18:03 -0400

    checking for vagrant pgsql user before trying to create it

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -2,9 +2,11 @@ apt_package "postgresql-9.1" do
   action :install
 end
 
-bash "created-pg-user-vagrant" do
-  user "postgres"
-  code <<-EOH
-  createuser --superuser vagrant
+execute "create-pg-user-vagrant" do
+  code = <<-EOH
+  psql -U postgres -c "select * from pg_user where usename='vagrant'" | grep -c vagrant
   EOH
+  user "postgres"
+  command "createuser --superuser vagrant"
+  not_if code, :user => 'postgres'
 end
