10627db99252326d9f68c6f8f69e7ab43b96d4d6
Jason Bouffard <jasonbouffard@gmail.com>
2013-06-16 12:00:03 -0400

    split the apt-get upgrade/update steps

--- cookbooks/pre-run/recipes/default.rb
+++ cookbooks/pre-run/recipes/default.rb
@@ -1,7 +1,14 @@
-bash "apt-get-upgrade" do
+bash "apt-get-update" do
   user "root"
   code <<-EOH
   apt-get -y update
+  EOH
+end
+
+bash "apt-get-dist-upgrade" do
+  user "root"
+  code <<-EOH
+  # https://github.com/mitchellh/vagrant/issues/289#issuecomment-12408200
   DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
   EOH
 end
