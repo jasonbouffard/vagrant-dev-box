b1af92988c1b4438a8e6feeab522ca7bfe1c1031
Jason Bouffard <jasonbouffard@gmail.com>
2013-06-16 20:28:35 -0400

    installing and enabling acl on os partition

--- cookbooks/pre-run/recipes/default.rb
+++ cookbooks/pre-run/recipes/default.rb
@@ -26,6 +26,20 @@ execute "rebuild-vbox-guest-additions" do
   not_if code
 end
 
+apt_package "acl" do
+  action :install
+end
+
+ruby_block "enable acl" do
+  block do
+    if File.readlines("/etc/fstab").grep(/defaults,acl/).size == 0
+      rc = Chef::Util::FileEdit.new("/etc/fstab")
+      rc.search_file_replace(/defaults/, "defaults,acl")
+      rc.write_file
+    end
+  end
+end
+
 directory node['project_dir'] do
   owner 'vagrant'
   group 'vagrant'
